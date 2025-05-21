d81a20b3b7a98fb280291eb4269ee680bfa698e0
Yuri Schulz <schulzyuri@gmail.com>
2025-05-21 20:31:24 -0300

    refactor: restructure Firebase configuration by removing deprecated firebaseAdminKey and updating initialization logic

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,17 @@
-import { initializeApp, cert } from 'firebase-admin/app';
+import {
+  initializeApp,
+  cert,
+  getApps,
+  ServiceAccount,
+} from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import * as serviceAccount from './firebaseAdminKey';
+import firebaseAdminKeys from './firebaseAdminKeys';
 
-initializeApp({
-  credential: cert(serviceAccount as any),
-});
+if (!getApps().length) {
+  initializeApp({
+    credential: cert(firebaseAdminKeys as ServiceAccount),
+    projectId: 'swiftsale-7e6bc',
+  });
+}
 
 export const firebaseAuth = getAuth();
