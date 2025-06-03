1d639e5c5722f98e6edd397162ec684e4afcad76
Yuri Schulz <schulzyuri@gmail.com>
2025-06-03 19:02:30 -0300

    fix: corrigir fluxo de autenticação para garantir que o próximo middleware seja chamado após a verificação do token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -16,7 +16,6 @@ export async function authenticateToken(
     jwt.verify(token, process.env.JWT_SECRET || '', (err, user) => {
       if (err) return res.sendStatus(403);
       req.user = user;
-      next();
     });
     next();
   } catch (error) {
