f9578956a8ff84945f4da9e31a40760263e06403
Yuri Schulz <schulzyuri@gmail.com>
2025-06-16 14:57:39 -0300

    refactor: adicionar suporte ao campo 'createdBy' nas operações de cliente

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,23 +1,34 @@
 import { createCustomerRequest } from '~/interfaces/customer';
 import { Customer } from '~/models/Customer';
 
-export async function getAllCustomers() {
-  return await Customer.find();
+export async function getAllCustomers(createdBy: string) {
+  return await Customer.find({ createdBy });
 }
 
-export async function createCustomer(data: createCustomerRequest) {
-  const customer = new Customer(data);
+export async function createCustomer(
+  data: createCustomerRequest,
+  createdBy: string
+) {
+  const customer = new Customer({ ...data, createdBy });
   return await customer.save();
 }
 
-export async function updateCustomer(id: string, data: createCustomerRequest) {
-  const updated = await Customer.findByIdAndUpdate(id, data, { new: true });
+export async function updateCustomer(
+  id: string,
+  data: createCustomerRequest,
+  createdBy: string
+) {
+  const updated = await Customer.findByIdAndUpdate(
+    { _id: id, createdBy },
+    data,
+    { new: true }
+  );
   if (!updated) return null;
   return updated;
 }
 
-export async function deleteCustomer(id: string) {
-  const deleted = await Customer.findByIdAndDelete(id);
+export async function deleteCustomer(id: string, createdBy: string) {
+  const deleted = await Customer.findByIdAndDelete({ _id: id, createdBy });
   if (!deleted) return null;
   return deleted;
 }
